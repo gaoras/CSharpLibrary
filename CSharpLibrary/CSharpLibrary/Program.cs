d73db6c89e914a2828d54eff7cbd964ad7256333
Yu <imok221@yahoo.co.jp>
2019-07-27 17:53:00 +0900

    csv読み取り関数作成

--- CSharpLibrary/CSharpLibrary/Program.cs
+++ CSharpLibrary/CSharpLibrary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibFileManagement;
 
 namespace CSharpLibrary
 {
@@ -10,6 +11,18 @@ namespace CSharpLibrary
     {
         static void Main(string[] args)
         {
+            //読み込みファイルのパスを指定
+            string filePath = @"C:\data\test.csv";
+            string[][] csvData = CsvReader.Read(filePath);
+            for (int i = 0; i < csvData.Length; i++)
+            {
+                for (int j = 0; j < csvData[i].Length; j++)
+                {
+                    Console.Write(csvData[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
--- CSharpLibrary/FileManagement/CsvReader.cs
+++ CSharpLibrary/FileManagement/CsvReader.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LibFileManagement
 {
     public class CsvReader
     {
+        public static string[][] Read(string path)
+        {
+            List<string[]> data = new List<string[]>();
+            try
+            {
+                //ファイルが存在するか確認
+                if (File.Exists(path) == true)
+                {
+                    using (StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift-JIS")))
+                    {
+                        //EOFまで読み取り
+                        while (sr.Peek() > -1)
+                        {
+                            data.Add(sr.ReadLine().Split(','));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return data.ToArray();
+        }
     }
 }
